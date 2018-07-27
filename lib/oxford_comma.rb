14daf07a6a485f332f8694f4ac6ac2e87631ef0d
Sharmaine Requerme <srqrme@gmail.com>
2018-07-27 02:10:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     "#{array[0]} and #{array[1]}"
-  elsif array.size >= 3
+  elsif array.length >= 3
     array[-1].insert(0, "and ")
     array.join(", ")
   end
