55f213da9c4a775c0fb92be1cd3ad686f8b10736
Sharmaine Requerme <srqrme@gmail.com>
2018-07-27 02:13:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.join
-  elsif array.size == 2
-    array.join(" and ")
-  elsif array.size >= 3
+  if array.size == 2
+    return array.join(" and ")
+  elsif array.length >= 3
     array[-1].insert(0, "and ")
     array.join(", ")
-  else
   end
+array.join(", ")
 end
